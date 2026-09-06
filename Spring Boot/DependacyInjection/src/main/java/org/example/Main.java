c76258e3056f84489ba337c61d8968505a638562
Ahad-demo <ahadmulani1405@gmail.com>
2026-09-06 16:05:41 +0530

    pop notification

--- Spring Boot/DependacyInjection/src/main/java/org/example/Main.java
+++ Spring Boot/DependacyInjection/src/main/java/org/example/Main.java
@@ -4,11 +4,12 @@ import org.example.Notifications.EmailServices;
 import org.example.Notifications.FakeServices;
 import org.example.Notifications.NotificationsServices;
 import org.example.Notifications.SmsServices;
+import org.example.Notifications.PopUpServiecs;
 
 public class Main {
     public static void main(String[] args) {
 
-        NotificationsServices notification = new FakeServices();
+        NotificationsServices notification = new PopUpServiecs();
 
         OrderServices order = new OrderServices(notification);
 
---
+++ Spring Boot/DependacyInjection/src/main/java/org/example/Notifications/PopUpServiecs.java
@@ -0,0 +1,9 @@
+package org.example.Notifications;
+
+public class PopUpServiecs implements NotificationsServices {
+
+    @Override
+    public void sendNotification() {
+        System.out.println("pop up notifications");
+    }
+}
