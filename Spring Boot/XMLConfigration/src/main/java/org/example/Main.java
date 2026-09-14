6c52c293f98f203cce64cf9b4e6dc5ff102b20ab
Ahad-demo <ahadmulani1405@gmail.com>
2026-09-14 07:16:59 +0530

    xml configaration

---
+++ Spring Boot/XMLConfigration/src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+
+        OrderServices orderServices = context.getBean("orderServices", OrderServices.class);
+
+        orderServices.order();
+    }
+}
---
+++ Spring Boot/XMLConfigration/src/main/java/org/example/OrderServices.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class OrderServices {
+
+    public void order() {
+        System.out.println("order done hogayiiiiiiiiii");
+    }
+}
